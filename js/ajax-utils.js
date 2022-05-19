8150b8980103fd12ce837e8e3986451866eddfe9
valentina Bustos <valebt23@hotmail.com>
2022-05-19 18:59:00 -0500

    cambio
    cambio

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -6,10 +6,10 @@ var ajaxUtils = {};
 
 // Returns an HTTP request object
 function getRequestObject() {
-  if (global.XMLHttpRequest) {
+  if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
-  else if (global.ActiveXObject) {
+  else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
@@ -64,3 +64,4 @@ global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
+
